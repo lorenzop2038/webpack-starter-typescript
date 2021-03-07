1dd5353c1d5a89d579fd5bcf94a868f5224bc52c
lorenzo_6toD_6 <lorenzo_perez_cemas@gmail.com>
2021-03-06 23:00:42 -0400

    04- Actualizar data en Firestore

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ const usuario = {
     fechaNaci: 0
 }
 
+const usuariosRef = db.collection('usuarios');
+
 // insert into usuarios....
 
 //db.collection('usuarios')
@@ -16,4 +18,19 @@ const usuario = {
 //    .catch( e => console.log('error', e ))
 
 
+//usuariosRef
+//      .doc('ZnvhfTEj3MhpDcewgNkh')
+//      .update({
+//          activo: true
+//      });
+
+// update usuarios set activo = false where.... 
+      usuariosRef
+      .doc('ZnvhfTEj3MhpDcewgNkh')
+      .set({
+          activo: true,
+          edad: 30,
+          casado: true,
+          id: 2131
+      });
 
