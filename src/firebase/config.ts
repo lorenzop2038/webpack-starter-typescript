e52a88c7f625c986b135e48f80cfdf7737794079
lorenzo_6toD_6 <lorenzo_perez_cemas@gmail.com>
2021-03-06 22:49:12 -0400

    03- Insertar data en Firestore

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,5 +1,5 @@
 import firebase from 'firebase/app';
-
+import 'firebase/firestore';
 
 const firebaseConfig = {
     apiKey: "AIzaSyCBDHgakFzHFnnGQ0szgh2YvGZgSIRwwK0",
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,19 @@
-import './firebase/config';
+import db from  './firebase/config';
+
+const usuario = {
+    nombre: 'Maria',
+    activo: true,
+    fechaNaci: 0
+}
+
+// insert into usuarios....
+
+//db.collection('usuarios')
+//    .add( usuario )
+//    .then( docRef => {
+//        console.log( docRef )
+//    })
+//    .catch( e => console.log('error', e ))
+
+
 
